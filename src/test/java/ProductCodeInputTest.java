b37d230e97b26cf7a6414633807f2499c3df9bd5
Daniil Dudak <daniildudak2002@gmail.com>
2022-11-20 18:39:07 +0300

    clean

--- src/test/java/ProductCodeInputTest.java
+++ src/test/java/ProductCodeInputTest.java
@@ -20,28 +20,23 @@ public class ProductCodeInputTest {
 
     @org.testng.annotations.Test
     public void productCodeInputTest() throws InterruptedException {
-        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"home\"]/div[1]/header/nav[1]/ul/li[3]/button"));
-
-//        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(4000));
-        Thread.sleep(4000);
-        searchButton.click();
+        String res = "";
         String codeOfProductStr = "VW0T0S12ZFRLC8";
+
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"home\"]/div[1]/header/nav[1]/ul/li[3]/button"));
+        searchButton.click();
         WebElement inputLine = driver.findElement(By.id("textSearch"));
         System.out.println(inputLine.getText());
-        Thread.sleep(4000);
+        Thread.sleep(2000);
         inputLine.sendKeys(codeOfProductStr);
         Thread.sleep(2000);
         WebElement product = driver.findElement(By.xpath("//*[@id=\"panelSearch\"]/div[2]/div[3]/form/div[3]/div/ul/li/a"));
-        Thread.sleep(2000);
         product.click();
-
         WebElement codeOfProduct = driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/div[4]/div[6]/div/div[3]/p"));
 
         String mydata = codeOfProduct.getAttribute("innerHTML");
         Pattern pattern = Pattern.compile("[A-Z0-9]{14}");
         Matcher matcher = pattern.matcher(mydata);
-
-        String res = "";
 
         res = matcher.find() ? matcher.group() : null;
 
